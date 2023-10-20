bd0a89f82584a92d4124013c307d9a859d96c5ca
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-10-20 00:25:33 -0300

    feat: install toastify and create initial structure

---
+++ src/components/home/Footer/index.tsx
@@ -0,0 +1,8 @@
+export function Footer(){
+
+    return (
+        <header>
+            <h1>Footer</h1>
+        </header>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import { Header } from "../components/home/Header";
+import { Footer } from "../components/home/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <ToastContainer autoClose={1500} />
+      <Footer />
+    </>
+  );
 }
